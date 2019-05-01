140842b949a15e09a6278d784e017f78242699d5
SasquatchXYZ <dalton.ricker.2011@graduate.usmma.edu>
2019-05-01 17:37:40 -0400

    working on final project, on MembershipManagementClass

--- src/JavaProject/FileHandler.java
+++ src/JavaProject/FileHandler.java
@@ -19,14 +19,60 @@ public class FileHandler {
         splitLine = lineRead.split(", ");
         if (splitLine[0].equals("S")) {
           // Instantiate a SingleClubMember
+          mem = new SingleClubMember('S',
+                  Integer.parseInt(splitLine[1]),
+                  splitLine[2],
+                  Double.parseDouble(splitLine[3]),
+                  Integer.parseInt(splitLine[4]));
         } else {
           // Instantiate a MultiClubMember
+          mem = new MultiClubMember('M',
+                  Integer.parseInt(splitLine[1]),
+                  splitLine[2],
+                  Double.parseDouble(splitLine[3]),
+                  Integer.parseInt(splitLine[4]));
         }
+        m.add(mem);
+        lineRead = reader.readLine();
       }
-    } catch {
+    } catch (IOException e) {
       // Code inside Catch Block
+      System.out.println(e.getMessage());
+    }
+    return m;
+  }
+
+  public void appendFile(String mem) {
+
+    try (BufferedWriter writer = new BufferedWriter((new FileWriter("members.csv", true)))) {
+      writer.write(mem + "\n");
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
+    }
+  }
+
+  public void overwriteFile(LinkedList<Member> m) {
+
+    String s;
+
+    try (BufferedWriter writer = new BufferedWriter((new FileWriter("members.temp", false)))) {
+      for (int k = 0; k < m.size(); k++) {
+        s = m.get(k).toString();
+        writer.write(s + "\n");
+      }
+    } catch (IOException e) {
+      System.out.println(e.getMessage());
     }
 
+    try {
+      File f = new File("members.csv");
+      File tf = new File("members.temp");
+
+      f.delete();
+      tf.renameTo(f);
+    } catch (Exception e) {
+      System.out.println(e.getMessage());
+    }
   }
 
 }
